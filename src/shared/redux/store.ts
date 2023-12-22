2a48aefbafa135476d23fd5f63abb7c35cd6c752
Vladimir Fedoseev <vladimir.fedoseev.2004@mail.ru>
2023-12-22 19:00:47 +0300

    add redux module

--- src/shared/redux/store.ts
+++ src/shared/redux/store.ts
@@ -1,1 +1,11 @@
-export {}
+import { createStore } from 'redux'
+
+const rootReducer = combineReducers({
+    title: titleReducer,
+    slides: slidesReducer,
+})
+
+let store = createStore(rootReducer)
+store.getState()
+
+export { store }
