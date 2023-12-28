68620bd50f3cf3ec1fd2d6c427852cfb4cc606df
Vladimir Fedoseev <vladimir.fedoseev.2004@mail.ru>
2023-12-28 21:22:39 +0300

    delete store from branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import { PresentationMaker } from './widgets/presentationMaker/ui'
-import { Provider } from 'react-redux'
-import { store } from './shared/redux/store'
 
-const App = () => (
-    <Provider store={store}>
-        <PresentationMaker />
-    </Provider>
-)
+const App = () => <PresentationMaker />
 
 export default App
--- src/shared/redux/store.ts
+++ src/shared/redux/store.ts
@@ -1,11 +1,1 @@
-import { createStore } from 'redux'
-
-const rootReducer = combineReducers({
-    title: titleReducer,
-    slides: slidesReducer,
-})
-
-let store = createStore(rootReducer)
-store.getState()
-
-export { store }
+export {}
